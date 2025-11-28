baeee1d2c8ad05d40ab86661fd17fb85eb906251
windylorbeta <windylorbeta@gmail.com>
2025-11-28 16:55:56 +0800

    Your commit message here

--- env/Scripts/activate.fish
+++ env/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'E:\MyFlask\env'
+set -gx VIRTUAL_ENV 'E:\MyFlask\env\env'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
